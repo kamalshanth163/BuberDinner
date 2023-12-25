717fe9c8888b9b4661e6fb332b189b5f0849efc8
kamal-maverick <kamalshanthdeveloper163@gmail.com>
2023-12-25 09:56:10 +0530

    Jwt token authentication and configuration

--- BuberDinner.Api/obj/Debug/net6.0/BuberDinner.Api.AssemblyInfo.cs
+++ BuberDinner.Api/obj/Debug/net6.0/BuberDinner.Api.AssemblyInfo.cs
@@ -10,6 +10,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("85bb3f47-4ec9-45fc-b9bd-b94393191042")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuberDinner.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
--- BuberDinner.Application/Services/Authentication/AuthenticationService.cs
+++ BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -1,7 +1,35 @@
+using BuberDinner.Application.Common.Interfaces;
+
 namespace BuberDinner.Application.Services.Authentication;
 
 public class AuthenticationService : IAuthenticationService
 {
+
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+    public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+    {
+        _jwtTokenGenerator = jwtTokenGenerator;
+    }
+
+    public AuthenticationResult Register(string firstName, string lastName, string email, string password)
+    {
+        // Check if user already exists
+
+        // Create user with unique Id
+
+        // Create JWT Token
+        var userId = Guid.NewGuid();
+        var token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName);
+
+        return new AuthenticationResult(
+            Guid.NewGuid(),
+            firstName,
+            lastName,
+            email,
+            token
+        );
+    }
     public AuthenticationResult Login(string email, string password)
     {
         return new AuthenticationResult(
@@ -13,14 +41,4 @@ public class AuthenticationService : IAuthenticationService
         );
     }
 
-    public AuthenticationResult Register(string firstName, string lastName, string email, string password)
-    {
-        return new AuthenticationResult(
-            Guid.NewGuid(),
-            firstName,
-            lastName,
-            email,
-            "token"
-        );
-    }
 }
